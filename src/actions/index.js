102df96880f89bad69eff1a316be111831a7f766
Aidan Jenkins <aidan.jenkins@gmail.com>
2021-08-18 17:21:38 -0700

    complete MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY"
+export const ADD_TO_MEMORY = "ADD_TO_MEMORY"
+export const MR_Button = "MR_BUTTON"
+export const MC_BUTTON = "MC_BUTTON"
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) => {
+    return ({type: CHANGE_OPERATION, payload: operator })
+}
+
+export const clearDisplay = () => {
+    return ({type: CLEAR_DISPLAY})
+}
+
+export const addToMemory = () => {
+    return ({type: ADD_TO_MEMORY})
+}
+
+export const mrButton = () => {
+    return ({type: MR_Button})
+}
+
+export const mcButton = () => {
+    return ({type: MC_BUTTON})
 }
